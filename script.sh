f4a9d707afde5dfdbe9072caf6c7cfa821eb9847
rraypure <raypurerahul99@gmail.com>
2025-11-14 09:41:59 +0000

    changes is script file

--- script.sh
+++ script.sh
@@ -8,13 +8,9 @@ echo "========================================================"
 sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 
-echo "========================================================"
-
 echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-
-echo "========================================================"
 
 sudo apt update
 sudo apt install jenkins -y
@@ -22,12 +18,24 @@ sudo apt install jenkins -y
 echo "Jenkins Installed"
 echo "========================================================"
 
-sudo apt update
-sudo apt install docker.io -y
-echo "Docker Installed"
-echo "========================================================"
+# sudo apt update
+# sudo apt install docker.io -y
+# echo "Docker Installed"
+# echo "========================================================"
+# sudo usermod -aG docker $USER
+# newgrp docker
+# sudo usermod -aG docker jenkins
+# echo "Done!!!"
+# echo "========================================================"
+
+
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo usermod -aG docker $USER
 newgrp docker
-sudo usermod -aG docker jenkins
-echo "Done!!!"
-echo "========================================================"
